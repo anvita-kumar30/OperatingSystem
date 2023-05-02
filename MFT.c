adebea58bad0deae77683534090a7806c5e518db
Anvita Kumar✨ <kumaranvita30@gmail.com>
2023-05-02 12:37:12 +0530

    init

--- MFT.c
+++ MFT.c
@@ -1,6 +1,6 @@
 //MFT (Multiprogramming with fixed number of tasks)
 #include <stdio.h>
-int main()
+int main() 
 {
     int tot, i, n, parsize, m, frag = 0;
     printf("Enter size of memory: ");
@@ -12,18 +12,18 @@ int main()
     scanf("%d", &n);
     int size[n];
     parsize = tot / n;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n; i++) 
     {
         printf("Enter process %d size: ", i + 1);
         scanf("%d", &size[i]);
-        if (size[i] <= parsize)
+        if (size[i] <= parsize) 
         {
             printf("Allocated page %d\n", i + 1);
             frag = frag + parsize - size[i];
-        }
-        else
+        } else 
+        {
             printf("Page %d is blocked as its size is larger than partition size\n", i + 1);
-        printf("\n");
+        }
     }
     printf("Internal fragmentation is %d", frag);
     return 0;
